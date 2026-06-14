fcd7e0402a620cde67c002bcc18aa665c6de51ee
MarcosAndradeNova <AndradeNovaMarcosDavid@gmail.com>
2026-06-13 20:28:20 -0400

    chore: limpieza de scripts de prueba temporales

--- check_pago_struct.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo json_encode(DB::select("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'pago'"));
--- check_user_status.php
+++
@@ -1,7 +0,0 @@
-<?php
-$user = \App\Models\User::where('email', 'andradenovamarcosdavid@gmail.com')->first();
-$postulante = \App\Models\Postulante::where('ciusuario', '1234567')->first();
-echo json_encode([
-    'user' => $user,
-    'postulante' => $postulante
-]);
